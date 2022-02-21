a7fb5f5d7aaedd3b430373060b2c662b1923925f
William Zeng <zeng.billy@gmail.com>
2022-02-21 14:50:17 -0800

    Drafted fillBoard method

--- Board.py
+++ Board.py
@@ -5,6 +5,8 @@ class Board:
     def __init__(self, length, rows) -> None:
         self.length = length
         self.board = [["" for i in range(length)] for j in range(rows)]
+        self.nextRow = 0
 
     def fillBoard(self, guess):
-        pass
+        self.board[self.nextRow] = [Letter(i) for i in guess]
+        self.nextRow += 1
